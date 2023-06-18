4b7b852529dc7b467554f95a316f70a607639901
KaysenGamiz <k.aiseen@hotmail.com>
2023-06-18 01:03:26 -0600

    Controllers added

---
+++ controllers/utils.js
@@ -0,0 +1,12 @@
+function getLatestRCC(){
+    return 1
+}
+
+function isEmpty(value){
+    if(value === '' || value === null || value === undefined){
+        return true
+    } 
+    else {
+        return false
+    }
+}
